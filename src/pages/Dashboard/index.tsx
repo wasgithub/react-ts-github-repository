507d2bbca358bb86147814fcec7b5325916ac79a
Washington Alexandre da Silva <was.alexandre42@gmail.com>
2020-11-22 11:30:23 -0300

    first commit

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <Title>Dashboard</Title>
+  )
+}
+
+export default Dashboard;
